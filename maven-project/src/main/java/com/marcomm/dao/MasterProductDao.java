56989c05c3db29f820e6ec5dcd51cd97f19d1dcb
diazf <diazf@DESKTOP-6QOTVEI>
2018-10-26 11:19:37 +0700

    Master Produk Dao

--- maven-project/src/main/java/com/marcomm/dao/MasterProductDao.java
+++ maven-project/src/main/java/com/marcomm/dao/MasterProductDao.java
@@ -6,8 +6,9 @@ import com.marcomm.model.MasterProduct;
 
 public interface MasterProductDao {
 public void save(MasterProduct masterProduct);
-public List<MasterProduct> getAll(MasterProduct masterProduct);
+public List<MasterProduct> getAll();
 public String getCodeById();
+public MasterProduct getById(int id);
 public void delete(MasterProduct masterProduct);
 public void update(MasterProduct masterProduct);
 }
