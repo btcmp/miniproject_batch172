0111776108d8e55ddc1411f63e955357de524fe6
'dragonremix' <'dragonremix911@gmail.com'>
2018-11-07 15:28:27 +0700

    tes commit

---
+++ src/main/java/com/marcomm/model/FormSouvenir.java
@@ -0,0 +1,23 @@
+package com.marcomm.model;
+
+import java.util.List;
+
+public class FormSouvenir {
+
+	private TransaksiSouvenir header;
+	private List<TransaksiSouvenirItem> detail;
+	public TransaksiSouvenir getHeader() {
+		return header;
+	}
+	public void setHeader(TransaksiSouvenir header) {
+		this.header = header;
+	}
+	public List<TransaksiSouvenirItem> getDetail() {
+		return detail;
+	}
+	public void setDetail(List<TransaksiSouvenirItem> detail) {
+		this.detail = detail;
+	}
+	
+	
+}
